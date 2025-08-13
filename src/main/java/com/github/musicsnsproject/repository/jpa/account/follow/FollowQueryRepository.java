537dc41668653efa41175d0bc730ce4297998e9d
sirimp <sihu89@daum.net>
2025-08-13 15:40:22 +0900

    f

--- src/main/java/com/github/musicsnsproject/repository/jpa/account/follow/FollowQueryRepository.java
+++ src/main/java/com/github/musicsnsproject/repository/jpa/account/follow/FollowQueryRepository.java
@@ -1,4 +1,18 @@
 package com.github.musicsnsproject.repository.jpa.account.follow;
 
+import java.util.List;
+
+import com.github.musicsnsproject.domain.follow.FollowVO;
+
 public interface FollowQueryRepository {
+	
+	// 나를 팔로우 하는 사람
+	List<FollowVO> findByFollowerAndUserInfo(String userId);
+	
+	// 내가 팔로우 하는 사람
+	List<FollowVO> findByFolloweeAndUserInfo(String userId);
+	
+	// 함께 아는 친구
+	List<FollowVO> findCommonFriend(String userId);
+	
 }
